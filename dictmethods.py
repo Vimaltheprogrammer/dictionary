0e1cf7c46c8ac33d1546654581feb643c11d7930
Vimaltheprogrammer <vimal1.yadav@gmail.com>
2021-05-30 19:08:00 +0530

    updated dictionary method

---
+++ dictmethods.py
@@ -0,0 +1,15 @@
+
+myDict = {"Vimal": 'A Beginer',
+"Manoj":'A salesperson',
+"list":[1,5,10,45,8],
+"andict": {"umesh":'master of coding'} 
+}
+# print(myDict.keys()) # print the keys of dictionery
+# print(myDict.values()) # print all values of the dictionery
+# print(myDict.items()) #cprint all (key + value)all content of dictionery
+# print(myDict)
+# updatedict = {
+#     "Maheep":"Chamadi"
+# }
+# myDict.update(updatedict) # to update a dictonery
+print(myDict.get("Vimal"))
